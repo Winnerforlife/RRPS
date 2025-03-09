29968173f8cecf48fa5f0f81b8759db02e4fe0b2
stanislav <1stanislavshevchuk@gmail.com>
2025-03-09 20:12:47 +0100

    feat: Added import-export module for RefundRequest admin

--- apps/refunds/admin.py
+++ apps/refunds/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
-
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 from apps.refunds.models import RefundRequest
 
 
+class RefundRequestResource(resources.ModelResource):
+    class Meta:
+        model = RefundRequest
+        fields = (
+            "id",
+            "user",
+            "order_number",
+            "order_date",
+            "first_name",
+            "last_name",
+            "phone_number",
+            "email",
+            "country",
+            "address",
+            "postal_code",
+            "city",
+            "products",
+            "reason",
+            "bank_name",
+            "account_type",
+            "iban",
+            "iban_verified",
+            "created_at",
+            "updated_at",
+            "status",
+        )
+
+
 @admin.register(RefundRequest)
-class RefundRequestAdmin(admin.ModelAdmin):
+class RefundRequestAdmin(ImportExportModelAdmin):
+    resource_class = RefundRequestResource
     list_display = ("id", "order_number", "status", "created_at")
     list_filter = ("status", "created_at", "country")
     search_fields = ("order_number", "email", "phone_number")

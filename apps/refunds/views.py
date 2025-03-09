2a5995973b59532dd94cdeb50608662d3bc188bc
stanislav <1stanislavshevchuk@gmail.com>
2025-03-09 17:29:58 +0100

    feat: Added basic functionality for users and for working with refunds, also added templates

--- apps/refunds/views.py
+++ apps/refunds/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, ListView, DetailView
 
-# Create your views here.
+from apps.refunds.forms import RefundRequestForm
+from apps.refunds.models import RefundRequest
+
+
+class CreateRefundRequestView(LoginRequiredMixin, CreateView):
+    model = RefundRequest
+    form_class = RefundRequestForm
+    template_name = 'refunds/create.html'
+    success_url = reverse_lazy('refund_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class RefundRequestListView(LoginRequiredMixin, ListView):
+    model = RefundRequest
+    template_name = 'refunds/list.html'
+    context_object_name = 'refund_requests'
+
+    def get_queryset(self):
+        return RefundRequest.objects.filter(user=self.request.user)
+
+
+class RefundRequestDetailView(LoginRequiredMixin, DetailView):
+    model = RefundRequest
+    template_name = 'refunds/detail.html'
+    context_object_name = 'refund_request'
+
+    def get_queryset(self):
+        return RefundRequest.objects.filter(user=self.request.user)
--- refund_project/urls.py
+++ refund_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("apps.accounts.urls")),
+    path("refunds/", include("apps.refunds.urls")),
 ]
